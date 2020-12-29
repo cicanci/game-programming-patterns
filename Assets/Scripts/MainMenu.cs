64cb5a37762e1ec27f92069f68324c0098db06b3
Bruno Cicanci <brunocicanci@gmail.com>
2020-12-29 17:54:02 +0000

    Adjusted main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SampleScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public void ChangeScene(int sceneIndex)
     {
